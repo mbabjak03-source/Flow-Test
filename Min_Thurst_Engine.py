b0d66c688fc7413949deda1d7b51276439e164d4
mbabjak03-source <mbabjak03@gmail.com>
2025-09-01 17:01:04 +0200

    Update Min_Thurst_Engine.py

--- Min_Thurst_Engine.py
+++ Min_Thurst_Engine.py
@@ -1,6 +1,6 @@
 
 g = 9.81
-vehicle_mass = 28000
+vehicle_mass = 43000
 trust_to_weight = 1.2
 number_of_engnines_S1 = 9
 
@@ -8,3 +8,4 @@ min_thrust_engine = g*vehicle_mass*trust_to_weight/number_of_engnines_S1
 
 
 min_thrust_output = min_thrust_engine
+
