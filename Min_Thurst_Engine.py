db96695651924cdbdffeec77dc77091d6302c681
mbabjak03-source <mbabjak03@gmail.com>
2025-09-01 16:57:26 +0200

    Add files via upload

---
+++ Min_Thurst_Engine.py
@@ -0,0 +1,9 @@
+
+g = 9.81
+vehicle_mass = 1000
+trust_to_weight = 1.2
+number_of_engnines_S1 = 9
+
+min_thrust_engine = g*vehicle_mass*trust_to_weight/number_of_engnines_S1
+
+min_thrust_output = min_thrust_engine
