09e86c9598df02934dd4dc7ba5b88237f5c5ebe4
mbabjak03-source <mbabjak03@gmail.com>
2025-09-07 10:36:05 +0200

    Update Tank Volumes.py

--- Tank Volumes.py
+++ Tank Volumes.py
@@ -3,12 +3,12 @@
 rof_S1 = 2.2
 rof_S2 = 2.1
 
+rho_ox = 1050
+
 m_ox_S1 = m_prop_S1*(rof_S1/(1+rof_S1))
 m_ox_S2 = m_prop_S1*(rof_S2/(1+rof_S2))
 
-m_fu_S1 = m_prop_S1 - m_ox_S1
-#m_fu_S2 = m_prop_S2 - m_ox_S2
-
 
 v_ox_tank_S1 = m_ox_S1/rho_ox
+
 
